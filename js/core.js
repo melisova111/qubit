500e4dde78f1813973b8c07e7ce327271bef811d
melisova111 <aytunukmirlanovna@gmail.com>
2023-12-29 14:48:08 +0600

    finished

--- js/core.js
+++ js/core.js
@@ -1,18 +1,75 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+// const hamburger = document.querySelector(".hamburger");
+// const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener("click", mobileMenu);
+// hamburger.addEventListener("click", mobileMenu);
 
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
+// function mobileMenu() {
+//     hamburger.classList.toggle("active");
+//     navMenu.classList.toggle("active");
+// }
 
-const navLink = document.querySelectorAll(".nav-link");
+// const navLink = document.querySelectorAll(".nav-link");
 
-navLink.forEach(n => n.addEventListener("click", closeMenu));
+// navLink.forEach(n => n.addEventListener("click", closeMenu));
 
-function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}
+// function closeMenu() {
+//     hamburger.classList.remove("active");
+//     navMenu.classList.remove("active");
+// }
+
+
+
+(function () {
+
+    let counter = document.querySelectorAll('.counter');
+    let limit = 0; // Переменная, чтобы останавливать функцию, когда всё запустится.
+    window.addEventListener('scroll', function () {
+        if (limit == counter.length) { return; }
+
+        for (let i = 0; i < counter.length; i++) {
+            let pos = counter[i].getBoundingClientRect().top; //Позиция блока, считая сверху окна
+            let win = window.innerHeight - 40; // На 40 пикселей меньше, чем высота окна
+            if (pos < win && counter[i].dataset.stop === "0") {
+                counter[i].dataset.stop = 1; // Останавливаем перезапуск счета в этом блоке
+                let x = 0;
+                limit++; // Счетчик будет запущен, увеличиваем переменную на 1
+                let int = setInterval(function () {
+                    // Раз в 60 миллисекунд будет прибавляться 50-я часть нужного числа
+                    x = x + Math.ceil(counter[i].dataset.to / 50);
+                    counter[i].innerText = x;
+                    if (x > counter[i].dataset.to) {
+                        //Как только досчитали - стираем интервал.
+                        counter[i].innerText = counter[i].dataset.to;
+                        clearInterval(int);
+                    }
+                }, 180);
+            }
+        }
+    });
+
+})();
+
+
+
+
+// const swiper = document.querySelector('.swiper').swiper;
+
+// // Now you can use all slider methods like
+// swiper.slideNext();
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
+
+
+
+
+

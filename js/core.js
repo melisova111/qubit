eb326f491739284e434885619d43ba56f68b0e49
melisova111 <aytunukmirlanovna@gmail.com>
2024-01-08 14:12:55 +0600

    finish

--- js/core.js
+++ js/core.js
@@ -70,6 +70,16 @@ var swiper = new Swiper(".mySwiper", {
   });
 
 
-
-
-
+// slider
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    mousewheel: true,
+    keyboard: true,
+  });
